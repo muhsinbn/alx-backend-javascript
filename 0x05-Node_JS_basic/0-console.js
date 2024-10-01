b24059d0b70bf4c1d27592a4e4bf799b42e2d136
Mohammed Yahaya <muhsinbnyahya@gmail.com>
2024-10-01 07:09:12 +0100

    code

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,6 @@
+// A Function that prints to the STDOUT
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+// A program that will be executed through command line
+
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+
+  if (chunk) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
